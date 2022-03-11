e5e41f669d39cdfde913f9d8837215d3898294f9
François <Schmitt.francois.1699@gmail.com>
2022-03-11 18:21:34 +0100

    url ok and index page ok

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,35 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { id, name, portrait, city, tagline, price, country } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/images/photographers/${portrait}`;
+    const url = `photographer.html?=${id}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = document.createElement('article');
+
+        let a = document.createElement('a');
+        article.appendChild(a);
+        a.title = "my title text";
+        a.href = url;
+
+
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const h2 = document.createElement('h2');
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        const paragrapheTagLine = document.createElement('p');
+        const paragrapheCountry = document.createElement('p');
+        const paragraphePrice = document.createElement('p');
+        paragrapheCountry.textContent = city + ", " + country;
+        paragrapheTagLine.textContent = tagline;
+        paragraphePrice.textContent = price + "€/jour";
+
+        a.appendChild(img);
+        a.appendChild(h2);
+        article.appendChild(paragrapheCountry);
+        article.appendChild(paragrapheTagLine);
+        article.appendChild(paragraphePrice);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { id, name, picture, tagline, city, price, country, getUserCardDOM }
 }
