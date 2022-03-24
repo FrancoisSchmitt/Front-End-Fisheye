bce5388be1f929b07f3baeadb0a729fdb3fb0f45
François <Schmitt.francois.1699@gmail.com>
2022-03-24 20:06:59 +0100

    add carousel

--- scripts/factories/galery.js
+++ scripts/factories/galery.js
@@ -8,8 +8,6 @@ function galeryFactory(data) {
 
 
     function getUserGaleryDOM() {
-
-
         const galeryUser = document.createElement("article");
         const userImage = document.createElement('div');
         const img = document.createElement('img');
@@ -20,28 +18,72 @@ function galeryFactory(data) {
         heart.classList.add("fa-solid", "fa-heart");
         likesParagraph.textContent = likes;
         titleParagraph.textContent = title;
-
         if (image) {
             img.setAttribute("alt", "img");
             img.setAttribute("src", galeryId);
             img.classList.add("lol");
+            img.setAttribute("onclick", "OpenCarousel()");
             galeryUser.appendChild(img);
         }
         else {
             videoGalery.setAttribute("type", "video/mp4");
             videoGalery.setAttribute("src", galeryvideoId);
             videoGalery.classList.add("wut");
-            videoGalery.setAttribute("controls", null);
+            // videoGalery.setAttribute("controls", null);
             galeryUser.appendChild(videoGalery);
         }
         galeryUser.appendChild(userImage);
         userImage.appendChild(titleParagraph);
         userImage.appendChild(likesParagraph);
         likesParagraph.appendChild(heart);
-
-
         return (galeryUser);
     }
-    return { id, photographerId, title, image, video, likes, date, price, getUserGaleryDOM }
+
+    function getLikeCounterDOM() {
+
+        const likesInfo = document.createElement("div");
+        const textLikeInfo = document.createElement('p');
+
+        var total;
+        for (let i = 0; i < likes; ++i) {
+            total += likes[i];
+            console.log(" le nombre total de likes est de" + " " + total);
+            // console.log(total);
+            textLikeInfo.textContent = total;
+            likesInfo.appendChild(textLikeInfo);
+        }
+        return (likesInfo);
+    }
+
+
+    function getGaleryUserCarouselDOM() {
+        const divContent = document.createElement("div");
+        divContent.classList.add("carousel-content-card");
+        const divImage = document.createElement("div");
+        divImage.classList.add("carousel-content-image");
+        const divTitle = document.createElement("div");
+        divTitle.classList.add("carousel-content-title");
+        const img = document.createElement('img');
+        const videoGalery = document.createElement("video");
+        divContent.appendChild(divImage);
+        if (image) {
+            img.setAttribute("alt", "img");
+            img.setAttribute("src", galeryId);
+
+            divImage.appendChild(img);
+        }
+        else {
+            videoGalery.setAttribute("type", "video/mp4");
+            videoGalery.setAttribute("src", galeryvideoId);
+
+            videoGalery.setAttribute("controls", null);
+            divImage.appendChild(videoGalery);
+        }
+        divContent.appendChild(divTitle);
+        return (divContent);
+
+    }
+
+    return { id, photographerId, title, image, video, likes, date, price, getUserGaleryDOM, getLikeCounterDOM, getGaleryUserCarouselDOM }
 }
 
--- scripts/pages/galery.js
+++ scripts/pages/galery.js
@@ -12,12 +12,21 @@ const idGalery = paramsGalery.get("id");
 
 async function displayDataMedia(mediaPhotographers) {
     const mediaHeader = document.querySelector(".photograph-media");
+    const countLike = document.querySelector(".likeCount-media");
+    const galeryCarousels = document.querySelector(".carousel-content");
 
     mediaPhotographers.forEach((media) => {
         if (media.photographerId == id) {
             const photographerModel = galeryFactory(media);
+
             const userGaleryCardDOM = photographerModel.getUserGaleryDOM();
             mediaHeader.appendChild(userGaleryCardDOM);
+
+            const likeCounterDOM = photographerModel.getLikeCounterDOM();
+            countLike.appendChild(likeCounterDOM);
+
+            const galeryUserCarousel = photographerModel.getGaleryUserCarouselDOM();
+            galeryCarousels.appendChild(galeryUserCarousel);
         }
     });
 };
@@ -30,38 +39,3 @@ async function init() {
 };
 
 init();
-
-//Mettre le code JavaScript lié à la page photographer.html
-// const getMedia = async () => {
-//     await fetch("./data/photographers.json")
-//         .then((res) => res.json())
-//         .then((data) => (media = data.media));
-//     return {
-//         media: [...media],
-//     };
-// };
-
-// const paramsMedia = new URLSearchParams(window.location.search);
-// const idMedia = paramsMedia.get("id");
-
-// async function displayDataMedia(media) {
-//     const mediaHeader = document.querySelector(".");
-
-//     media.forEach((media) => {
-
-//         if (media.photographerId == idMedia) {
-//             const mediaModel = galeryFactory(media);
-//             const userBannerCardDOM = mediaModel.getUserGaleryDOM();
-//             mediaHeader.appendChild(userBannerCardDOM);
-//         }
-//     });
-// };
-
-// async function init() {
-//     // Récupère les datas des photographes
-//     const { media } = await getMedia();
-//     displayDataMedia(media);
-// };
-
-// init();
-
